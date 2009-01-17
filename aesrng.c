3949fc34c5f3918fb7686ac8d813af3c1720f737
Christopher Wellons <mosquitopsu@gmail.com>
2009-01-16 23:52:24 -0500

    Oops, we were already in CBC mode, so we were doing it twice.

--- aesrng.c
+++ aesrng.c
@@ -38,22 +38,17 @@ int main (int argc, char **argv)
   gcry_cipher_setkey (aes, key, 32);
   gcry_cipher_setiv (aes, iv, 16);
 
-  unsigned char in[16], out[16];
-  gcry_create_nonce (&out[0], 16);
-
   /* Initialize the counter */
   unsigned char counter_iv[16];
   gcry_create_nonce (&counter_iv[0], 16);
   gcry_mpi_t counter = gcry_mpi_new (16);
   gcry_mpi_scan (&counter, GCRYMPI_FMT_USG, &counter_iv[0], 16, NULL);
 
+  unsigned char in[16], out[16];
   while (bytes)
     {
       gcry_mpi_add_ui (counter, counter, 1);
       gcry_mpi_print (GCRYMPI_FMT_USG, &in[0], 16, NULL, counter);
-      int i;
-      for (i = 0; i < 16; i++)
-	in[i] ^= out[i];
       gcry_cipher_encrypt (aes, &out[0], 16, &in[0], 16);
 
       /* Print out the bytes. */
